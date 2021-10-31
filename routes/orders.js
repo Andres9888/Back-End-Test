1ee9e41cdf8f230c2bb80ba20caf320ffe76bcbc
Andres9888 <Andres9888@Gmail.com>
2021-10-31 10:52:37 -0400

    fix: :bug: fix update route allowing customer name to updated only if no items

--- routes/orders.js
+++ routes/orders.js
@@ -1,104 +1,94 @@
-const Router = require("koa-router")
-const uuid = require("uuid").v4
+const Router = require('koa-router');
+const uuid = require('uuid').v4;
 
-const ordersRouter = new Router({ prefix: "/orders" })
-const ordersData = require("../data/ordersData")
+const ordersRouter = new Router({ prefix: '/orders' });
+const ordersData = require('../data/ordersData');
 
-ordersRouter.post("/", async (ctx) => {
-  const { customerName, items } = ctx.request.body
+ordersRouter.get('/', async (ctx) => {
+  const { filterProperty, filterValue } = ctx.query;
 
-  if (!items.length) {
-    ctx.throw(409, "No items ordered")
+  let results = ordersData;
+
+  if (filterProperty && filterValue) {
+    const filteredResults = ordersData.filter(({ items }) =>
+      items.some(({ name }) => name.includes(filterValue))
+    );
+    results = filteredResults;
   }
 
-  const total = items.reduce(
-    (orderTotal, item) => (orderTotal += item.price),
-    0
-  )
+  ctx.status = 200;
+  ctx.body = results;
+});
+
+ordersRouter.get('/:id', async (ctx) => {
+  const { id } = ctx.params;
+  const order = ordersData.find((order) => order.id === id);
+
+  if (!order) {
+    ctx.throw(404, 'Order not found');
+  }
+
+  ctx.status = 200;
+  ctx.body = order;
+});
+
+ordersRouter.post('/', async (ctx) => {
+  const { customerName, items } = ctx.request.body;
+
+  if (!items.length) {
+    ctx.throw(409, 'No items ordered');
+  }
+
+  const total = items.reduce((orderTotal, item) => (orderTotal += item.price), 0);
   const order = {
     id: uuid(),
     customerName,
     createdOn: new Date(),
     items,
     total,
-  }
+  };
 
-  ctx.status = 201
-  ctx.body = [...ordersData, order]
-})
+  ctx.status = 201;
+  ctx.body = [...ordersData, order];
+});
 
-ordersRouter.get("/", async (ctx) => {
-  const { filterProperty, filterValue } = ctx.query
+ordersRouter.put('/:id', async (ctx) => {
+  const { id } = ctx.params;
+  const { customerName, items } = ctx.request.body;
 
-  let results = ordersData
-
-  if (filterProperty && filterValue) {
-    const filteredResults = ordersData.filter(({ items }) =>
-      items.some(({ name }) => name.includes(filterValue))
-    )
-    results = filteredResults
-  }
-
-  ctx.status = 200
-  ctx.body = results
-})
-
-ordersRouter.get("/:id", async (ctx) => {
-  const { id } = ctx.params
-  const order = ordersData.find((order) => order.id === id)
+  let order = ordersData.find((order) => order.id === id);
 
   if (!order) {
-    ctx.throw(404, "Order not found")
+    ctx.throw(404, 'Could not find order');
+  }
+  if (!items && !customerName) {
+    ctx.throw(400, 'No items ordered or Name to update');
+  }
+  if (customerName) {
+    order['customerName'] = customerName;
+  }
+  if (items) {
+    order['items'] = items;
+    order['price'] = items.reduce((orderTotal, item) => (orderTotal += item.price), 0);
   }
 
-  ctx.status = 200
-  ctx.body = order
-})
+  ctx.status = 200;
+  ctx.body = { ...order };
+});
 
-ordersRouter.put("/:id", async (ctx) => {
-  const { id } = ctx.params
-  const { customerName, items } = ctx.request.body
+ordersRouter.delete('/:id', async (ctx) => {
+  const { id } = ctx.params;
 
-  let order = ordersData.find((order) => order.id === id)
+  const order = ordersData.find((order) => order.id === id);
 
   if (!order) {
-    ctx.throw(404, "Could not find order")
+    ctx.throw(404, 'Could not find order');
   }
 
-  if (!items) {
-    ctx.throw(409, "No items ordered")
-  } else {
-    order["items"] = items
-    order["price"] = items.reduce(
-      (orderTotal, item) => (orderTotal += item.price),
-      0
-    )
-  }
+  const remaining = ordersData.filter((order) => order.id !== id);
 
-  console.log(order)
-  const updated = {
-    ...order,
-    //customerName,
-    //items,
-  }
+  ctx.status = 200;
+  ctx.body = remaining;
+});
 
-  ctx.status = 200
-  ctx.body = updated
-})
-
-ordersRouter.delete("/:id", async (ctx) => {
-  const { id } = ctx.params
-
-  const order = ordersData.find((order) => order.id === id)
-
-  if (!order) {
-    ctx.throw(404, "Could not find order")
-  }
-
-  const remaining = ordersData.filter((order) => order.id !== id)
-
-  ctx.status = 200
-  ctx.body = remaining
-})
-
-module.exports = ordersRouter
+module.exports = ordersRouter;

399cce33d9f7b2b4cf1ee45561c1e2408186aaad
Andres9888 <Andres9888@Gmail.com>
2021-11-01 10:27:33 -0400

    refactor: :art: Fix delete and add better error handling

--- TICKETS.md
+++ TICKETS.md
@@ -44,6 +44,63 @@ I checked a few times to make sure I was adding the order prices correctly. The 
  
 We might have to review this to make sure the items listed are correct. More than likely you want a similar item and got it missed up. For example Cheeseburger with Cheese Pizza or something like this. 
 
+This is the data I used
+
+{
+  "customerName": "family of six",
+  "items": [
+    {
+      "id": "91bf040b-dd2c-4bd7-a52a-612be3ed4589",
+      "name": "Cheeseburger",
+      "size": null,
+      "quantity": 2,
+      "price": 1090
+    },
+    {
+      "id": "d6f9b082-b959-4067-a481-c42bf808e531",
+      "name": "Hawaiian Pizza",
+      "size": "medium",
+      "quantity": 1,
+      "price": 2295
+    },
+    {
+      "id": "c843d936-9772-4391-9055-701a580073d2",
+      "name": "Additional Topping - Pickles",
+      "size": null,
+      "quantity": 2,
+      "price": 50
+    },
+    {
+      "id": "e361c0d6-a369-4c2a-b917-f792acf73d11",
+      "name": "Fiesta Salad",
+      "size": "large",
+      "quantity": 1,
+      "price": 1550
+    },
+    {
+      "id": "24a8a3e5-ccb7-4f9c-9497-48f924fbf438",
+      "name": "Additional Topping - Avocado",
+      "size": null,
+      "quantity": 3,
+      "price": 375
+    },
+    {
+      "id": "23b855da-cbf9-42ec-933f-91db213b1849",
+      "name": "French Fries",
+      "size": "medium",
+      "quantity": 3,
+      "price": 433
+    },
+    {
+      "id": "cddbc9c5-748f-476d-83e2-f71f1ac82731",
+      "name": "Fountain Drink",
+      "size": "large",
+      "quantity": 4,
+      "price": 598
+    }
+  ]
+}
+
 ## Updating An Order
 ### QA Notes
 When getting updating an order I expect to only have to pass what has changed. However, if I don't pass everything (customerName or items), that value gets removed. If for instance I did not change the customer name, I would expect it to use the one originally on the order.
@@ -52,6 +109,7 @@ Additionally, when updating the items ordered, the total is not updating.
 
 ### Dev Notes / Response
 
+Should be fixed.
 
 ---
 
@@ -62,6 +120,7 @@ When  I delete an order, the order that gets deleted is never the one I expect. 
 
 ### Dev Notes / Response
 
+Should be fixed I deleted the actual order but could have just set it to delete = true status to keep the data until we actually want to delete it and not show it. 
 
 ---
 
--- routes/orders.js
+++ routes/orders.js
@@ -6,35 +6,45 @@ const ordersData = require("../data/ordersData")
 
 ordersRouter.get("/", async (ctx) => {
   const { filterProperty, filterValue } = ctx.query
+  try {
+    let results = ordersData
 
-  let results = ordersData
-
-  if (filterProperty && filterValue) {
-    if (filterProperty === "name") {
-      results = ordersData.filter(({ items }) =>
-        items.some(({ name }) => name.includes(filterValue))
-      )
-    } else {
-      results = ordersData.filter(
-        (order) => order[filterProperty] === filterValue
-      )
+    if (filterProperty && filterValue) {
+      if (filterProperty === "name") {
+        results = ordersData.filter(({ items }) =>
+          items.some(({ name }) => name.includes(filterValue))
+        )
+      } else {
+        results = ordersData.filter(
+          (order) => order[filterProperty] === filterValue
+        )
+      }
     }
-  }
 
-  ctx.status = 200
-  ctx.body = results
+    ctx.status = 200
+    ctx.body = results
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = error
+  }
 })
 
 ordersRouter.get("/:id", async (ctx) => {
   const { id } = ctx.params
-  const order = ordersData.find((order) => order.id === id)
 
-  if (!order) {
-    ctx.throw(404, "Order not found")
+  try {
+    const order = ordersData.find((order) => order.id === id)
+
+    if (!order) {
+      ctx.throw(404, "Order not found")
+    }
+
+    ctx.status = 200
+    ctx.body = order
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = error
   }
-
-  ctx.status = 200
-  ctx.body = order
 })
 
 ordersRouter.post("/", async (ctx) => {
@@ -44,62 +54,75 @@ ordersRouter.post("/", async (ctx) => {
     ctx.throw(409, "No items ordered")
   }
 
-  const total = items.reduce(
-    (orderTotal, item) => (orderTotal += item.price),
-    0
-  )
-  const order = {
-    id: uuid(),
-    customerName,
-    createdOn: new Date(),
-    items,
-    total,
-  }
+  try {
+    const total = items.reduce(
+      (orderTotal, item) => (orderTotal += item.price),
+      0
+    )
 
-  ctx.status = 201
-  ctx.body = [...ordersData, order]
+    const order = {
+      id: uuid(),
+      customerName,
+      createdOn: new Date(),
+      items,
+      total,
+    }
+
+    ctx.status = 201
+    ctx.body = [...ordersData, order]
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = error
+  }
 })
 
 ordersRouter.put("/:id", async (ctx) => {
   const { id } = ctx.params
   const { customerName, items } = ctx.request.body
 
-  let order = ordersData.find((order) => order.id === id)
+  try {
+    let order = ordersData.find((order) => order.id === id)
 
-  if (!order) {
-    ctx.throw(404, "Could not find order")
-  }
-  if (!items && !customerName) {
-    ctx.throw(400, "No items ordered or Name to update")
-  }
-  if (customerName) {
-    order["customerName"] = customerName
-  }
-  if (items) {
-    order["items"] = items
-    order["price"] = items.reduce(
-      (orderTotal, item) => (orderTotal += item.price),
-      0
-    )
-  }
+    if (!order) {
+      ctx.throw(404, "Could not find order")
+    }
+    if (!items && !customerName) {
+      ctx.throw(400, "No items ordered or Name to update")
+    }
+    if (customerName) {
+      order["customerName"] = customerName
+    }
+    if (items) {
+      order["items"] = items
+      order["price"] = items.reduce(
+        (orderTotal, item) => (orderTotal += item.price),
+        0
+      )
+    }
 
-  ctx.status = 200
-  ctx.body = { ...order }
+    ctx.status = 200
+    ctx.body = { ...order }
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = error
+  }
 })
 
 ordersRouter.delete("/:id", async (ctx) => {
   const { id } = ctx.params
+  try {
+    const order = ordersData.find((order) => order.id === id)
 
-  const order = ordersData.find((order) => order.id === id)
+    if (!order) {
+      ctx.throw(404, "Could not find order")
+    }
 
-  if (!order) {
-    ctx.throw(404, "Could not find order")
+    ctx.status = 200
+    ctx.body = ordersData
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = error
   }
-
-  const remaining = ordersData.filter((order) => order.id !== id)
-
-  ctx.status = 200
-  ctx.body = remaining
 })
 
 module.exports = ordersRouter
